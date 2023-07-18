dfe9f29efcdd8ec7f0a4eebc278d712efd62da75
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-07-17 21:15:47 -0600

    s

--- src/components/FileCard.jsx
+++ src/components/FileCard.jsx
@@ -2,19 +2,31 @@ import React, { useState } from 'react';
 import { PropagateLoader } from 'react-spinners';
 
 const FileCard = ({ file, response }) => {
-  return (
+
+    // Calculate the print time in hours and price
+    let printTimeInHours;
+    let price;
+    if (response && response.timeToPrintInSeconds) {
+        printTimeInHours = response.timeToPrintInSeconds / 3600;
+        price = printTimeInHours; // $1 per hour
+    }
+
+    return (
     <div className="border border-gray-300 rounded shadow-md p-4 mb-4">
-      <h2 className="text-lg font-semibold mb-2">{file.name}</h2>
-      {response ? (
-        <p className="m-auto text-center">{JSON.stringify(response)}</p>
-      ) : (
-        <p className="m-auto text-center">Processing</p>
-      )}
-      <div className="flex justify-center mt-4">
-        <PropagateLoader color="#000000" size={10} />
-      </div>
+        <h2 className="text-lg font-semibold mb-2">{file.name}</h2>
+        {response ? (
+            <>
+                <p className="m-auto text-center">Estimated Print Time: {printTimeInHours.toFixed(2)} hours</p>
+                <p className="m-auto text-center">Estimated Price: ${price.toFixed(2)}</p>
+            </>
+        ) : (
+            <div className="m-auto text-center">
+                <p>Processing</p>
+                <PropagateLoader color="#000000" size={10} />
+            </div>
+        )}
     </div>
-  );
+    );
 };
 
 export default FileCard;
